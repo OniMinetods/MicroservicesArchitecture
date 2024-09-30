37d093fda26d7a5194046e146e91b571001a3399
OniMinetods <146983705+OniMinetods@users.noreply.github.com>
2024-09-30 20:15:02 +0300

    feat: added CreatePlatformRequest

--- Controllers/PlatformsController.cs
+++ Controllers/PlatformsController.cs
@@ -1,6 +1,7 @@
 ﻿using AutoMapper;
 using MicroservicesArchitecture.Data;
 using MicroservicesArchitecture.Dtos;
+using MicroservicesArchitecture.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace MicroservicesArchitecture.Controllers
@@ -39,6 +40,18 @@ namespace MicroservicesArchitecture.Controllers
             }
 
             return NotFound();
+        }
+
+        [HttpPost]
+        public ActionResult<PlatformReadDto> CreatePlatform(PlatformCreateDto platformCreateDto)
+        {
+            var platformModel = _mapper.Map<Platform>(platformCreateDto);
+            _repository.CreatePlatform(platformModel);
+            _repository.SaveChanges();
+
+            var platformReadDto = _mapper.Map<PlatformReadDto>(platformModel);
+
+            return CreatedAtRoute(nameof(GetPlatformById), new { Id = platformReadDto.Id }, platformReadDto);
         }
     }
 }
